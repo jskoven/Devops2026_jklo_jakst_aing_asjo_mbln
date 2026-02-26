cb5c08974fb5c20448831b63f11219370dd0ff39
Johan Skoven <jklo@itu.dk>
2026-02-26 16:28:05 +0100

    test

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -7,12 +7,13 @@ docker pull ${DOCKER_USERNAME}/minitwitimage:latest
 # 2. Stop and remove the old container
 docker stop minitwit || true
 docker rm minitwit || true
+docker image prune -f
 
 # 3. Run the new version
 # We mount /tmp to /data because Vagrant puts the DB in /tmp
 docker run -d \
   --name minitwit \
-  -p 80:5000 \
+  -p 80:5001 \
   -v /tmp:/data \
   -e "DATABASE_PATH=/data/minitwit.db" \
   ${DOCKER_USERNAME}/minitwitimage:latest
