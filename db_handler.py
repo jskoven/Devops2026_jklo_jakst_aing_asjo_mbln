14d548be9a5a2de8a311d5ff42e9a938219b9704
Asger124 <asjo@itu.dk>
2026-03-06 11:48:23 +0100

    refactor of db_handler to use SQLModel

--- db_handler.py
+++ db_handler.py
@@ -1,35 +1,50 @@
-import sqlite3
 import os
 from contextlib import closing
+from typing import Annotated
 
-DATABASE = os.getenv("DATABASE_PATH", "/tmp/minitwit.db")
+from fastapi import Depends
+from user import User
+from message import Message
+from sqlmodel import SQLModel, Session, create_engine
 
-def connect_db():
-    """Returns a new connection to the database."""
-    return sqlite3.connect(DATABASE, check_same_thread=False)
+# DATABASE = os.getenv("DATABASE_PATH", "/tmp/minitwit.db")
 
-def get_db():
-    db = connect_db()
-    db.row_factory = sqlite3.Row
-    try:
-        yield db
-    finally:
-        db.close()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./minitwit.db")
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# def init_db():
+#     """Creates the database tables."""
+#     with closing(connect_db()) as db:
+#         with open('schema.sql', mode='r', encoding='utf-8') as f:
+#             db.cursor().executescript(f.read())
+#         db.commit()
 
 def init_db():
-    """Creates the database tables."""
-    with closing(connect_db()) as db:
-        with open('schema.sql', mode='r', encoding='utf-8') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    SQLModel.metadata.create_all(engine)
 
-def query_db(db, query, args=(), one=False):
-    """Queries the database and returns a list of dictionaries."""
-    cur = db.execute(query, args)
-    rv = [dict(row) for row in cur.fetchall()]
-    return (rv[0] if rv else None) if one else rv
+def get_session():
+    with Session(engine) as session:
+        yield session
 
-def get_user_id(db, username):
-    rv = db.execute('select user_id from user where username = ?', [username]).fetchone()
-    return rv[0] if rv else None
+SessionDep = Annotated[Session, Depends(get_session)]
+
+# def get_db():
+#     db = connect_db()
+#     db.row_factory = sqlite3.Row
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
+
+# def query_db(db, query, args=(), one=False):
+#     """Queries the database and returns a list of dictionaries."""
+#     cur = db.execute(query, args)
+#     rv = [dict(row) for row in cur.fetchall()]
+#     return (rv[0] if rv else None) if one else rv
+
+# def get_user_id(db, username):
+#     rv = db.execute('select user_id from user where username = ?', [username]).fetchone()
+#     return rv[0] if rv else None
 
