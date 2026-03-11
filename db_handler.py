2667607971c13338e024e2e073c9636f1ffb544a
Johan Skoven <jklo@itu.dk>
2026-03-11 15:07:24 +0100

    Migrated to postgres droplet

--- db_handler.py
+++ db_handler.py
@@ -1,20 +1,18 @@
 import os
-from contextlib import closing
 from typing import Annotated
-
 from fastapi import Depends
 from user import User
 from message import Message
 from sqlmodel import SQLModel, Session, create_engine, select
-from sqlalchemy.pool import StaticPool
 
-DEFAULT_DB_URL = "/tmp/minitwit.db"
-DATABASE_PATH = os.getenv("DATABASE_PATH", DEFAULT_DB_URL)
-if not DATABASE_PATH.startswith("sqlite:///"):
-    sqlite_url = f"sqlite:///{DATABASE_PATH}"
-else: 
-    sqlite_url = DATABASE_PATH
-engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("No DATABASE_URL found in environment variables.")
+
+
+engine = create_engine(DATABASE_URL)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
@@ -25,9 +23,8 @@ def get_session():
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-def get_user_id(username,session:Session):
+def get_user_id(username, session: Session):
     """Convenience method to look up the id for a username."""
     statement = select(User.user_id).where(User.username == username)
     result = session.exec(statement).first()
-    return result 
-
+    return result
