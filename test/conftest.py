6d2e66352e2be7369b6bd72e008afaf94abb6289
Asger124 <asjo@itu.dk>
2026-03-19 14:37:02 +0100

    added configuration test file
    This file at each test session. Builds and tears down db-sessions

---
+++ test/conftest.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+import pytest
+from fastapi import Depends
+from sqlmodel import SQLModel, Session, create_engine
+
+TEST_DB_URL = "postgresql://minitwit:minitwit@db:5432/minitwit"
+GUI_URL = "http://minitwit:5001"
+engine = create_engine(TEST_DB_URL)
+
+@pytest.fixture(scope="session")
+def db_engine():
+    SQLModel.metadata.create_all(engine)
+    yield engine
+    SQLModel.metadata.drop_all(engine)
+    engine.dispose()
+
+@pytest.fixture
+def db_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(db_session)]
